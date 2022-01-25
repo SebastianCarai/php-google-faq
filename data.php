afde9e361c2623b52210153e7488a65d35dc60ed
SebastianCarai <sebastiancarai7@gmail.com>
2022-01-25 15:07:29 +0100

    base

---
+++ data.php
@@ -0,0 +1,44 @@
+<?php 
+    $faq_array =[
+        'faq1' => [
+            'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+            'answer' => [
+                'ciao',
+                'hola',
+                'hello'
+            ]
+        ],
+        'faq2' => [
+            'question' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+            'answer' => [
+                'ciao',
+                'hola',
+                'hello'
+            ]
+        ],
+        'faq3' => [
+            'question' => "Perché il mio account è associato a un paese?",
+            'answer' => [
+                'ciao',
+                'hola',
+                'hello'
+            ]
+        ],
+        'faq4' => [
+            'question' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+            'answer' => [
+                'ciao',
+                'hola',
+                'hello'
+            ]
+        ],
+        'faq5' => [
+            'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+            'answer' => [
+                'ciao',
+                'hola',
+                'hello'
+            ]
+        ],
+    ]
+?>
--- index.php
+++ index.php
@@ -6,13 +6,51 @@
     <meta name="viewport" content="width=ù, initial-scale=1.0">
     <title>Document</title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@400;600;800&family=Roboto:wght@400;500;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="data.php">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
     <!-- Header -->
     <header>
+        <img class="google_logo d-i_b" src="https://1000marche.net/wp-content/uploads/2020/03/Google-logo.png" alt="Google logo">
+        <a class="header_title d-i_b" href="#">Privacy e termini</a>
 
+        <!-- Header nav -->
+        <div>
+            <ul class="header_nav">
+                <li class="d-i_b"> <a class="d-i_b" href="#">Introduzione</a> </li>
+                <li class="d-i_b"> <a class="d-i_b" href="#">Norme sulla privacy</a> </li>
+                <li class="d-i_b"> <a class="d-i_b" href="#">Termini di servzio</a> </li>
+                <li class="d-i_b"> <a class="d-i_b" href="#">Tecnologie</a> </li>
+                <li class="d-i_b"> <a class="d-i_b active" href="#">Domande frequenti</a> </li>
+            </ul>
+        </div>
     </header>
+    <!-- End Header -->
+
+
+
+    <!-- Main -->
+    <main>
+        <div class="container">
+            <?php 
+            include 'data.php'; 
+            foreach($faq_array as $single_faq){
+            ?>
+                <div class="single_faq">
+                    <h2 class="faq_question"> <?php echo $single_faq['question']; ?> </h2>
+                    <?php foreach($single_faq['answer'] as $single_answer){ ?>
+                        <p class="faq_answer"> <?php echo $single_answer; ?> </p>
+                    <?php }; ?>
+                </div>
+            <?php } ?>
+        </div>
+    </main>
+    <!-- End Main -->
+
 </body>
 </html>
